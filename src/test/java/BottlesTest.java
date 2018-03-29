794f7b4e324268aa95ae97390db8cbf9975e6c87
3theend3@gmail.com <3theend3@gmail.com>
2018-03-29 15:47:24 +0200

    commit primo test

--- src/test/java/BottlesTest.java
+++ src/test/java/BottlesTest.java
@@ -2,18 +2,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class BottlesTest {
     @Test
-    public void testSong(){
+    public void testSong() throws IOException {
         final ByteArrayOutputStream stream = new ByteArrayOutputStream();
         Bottles.writeLyrics(
                 new PrintStream(
                         stream));
 
         final String actual = new String(stream.toByteArray());
+        final String expected = new String(Files.readAllBytes(Paths.get("src/main/resources/test.txt")));
 
-        Assert.assertNotNull(actual);
+        Assert.assertEquals(expected,actual);
     }
 }
