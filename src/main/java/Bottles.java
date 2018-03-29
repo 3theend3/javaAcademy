a72847716a35c642621a93de3bec09379a119c89
3theend3@gmail.com <3theend3@gmail.com>
2018-03-29 17:32:15 +0200

    replace bottle/bottles with container()

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -12,31 +12,36 @@ class Bottles
         {
             if (beers==0)
             {
-                out.print(beers + " bottles"  + " of beer on the wall,");
-                out.println(beers + " bottles"  + " of beer,");
+                out.print(beers + containers(beers) + " of beer on the wall,");
+                out.println(beers + containers(beers) + " of beer,");
                 out.print("Go to the store, buy some more,");
-                out.println("99 bottles of beer on the wall.\r\n");
+                out.println("99" + containers(beers) + " of beer on the wall.\r\n");
                 
             }
             else if(beers==1){
 
-                out.print(beers + " bottle"  + " of beer on the wall,");
-                out.println(beers + " bottle"  + " of beer,");
+                out.print(beers + containers(beers) + " of beer on the wall,");
+                out.println(beers + containers(beers) + " of beer,");
                 out.print("Take one down, pass it around,");
-                out.println(beers-1 + " bottles"  + " of beer on the wall.\r\n");
+                out.println(beers-1 + containers(beers-1) + " of beer on the wall.\r\n");
             }else if (beers - 1 == 1){
 
-                out.print(beers + " bottles" + " of beer on the wall,");
-                out.println(beers + " bottles" + " of beer,");
+                out.print(beers + containers(beers) + " of beer on the wall,");
+                out.println(beers + containers(beers) + " of beer,");
                 out.print("Take one down, pass it around,");
-                out.println(beers-1 + " bottle" + " of beer on the wall.\r\n");
+                out.println(beers-1 + containers(beers-1) + " of beer on the wall.\r\n");
             }
             else{
-                out.print(beers + " bottles" + " of beer on the wall,");
-                out.println(beers + " bottles" + " of beer,");
+                out.print(beers + containers(beers) + " of beer on the wall,");
+                out.println(beers + containers(beers) + " of beer,");
                 out.print("Take one down, pass it around,");
-                out.println(beers-1 + " bottles" + " of beer on the wall.\r\n");
+                out.println(beers-1 + containers(beers-1) + " of beer on the wall.\r\n");
             }
         }
     }
+
+    private static String containers(int n){
+        return n == 1 ? " bottle" : " bottles";
+    }
+
 }
