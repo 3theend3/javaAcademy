3658b855aa167462bba438742f55e9d1a1908e06
3theend3@gmail.com <3theend3@gmail.com>
2018-03-29 16:22:07 +0200

    every branch has his own complete phrase

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -22,19 +22,23 @@ class Bottles
             else
             {
 
-
-                out.print(beers + " bottle" + s + " of beer on the wall,");
-                out.println(beers + " bottle" + s + " of beer,");
-                out.print("Take one down, pass it around,");
-
                 if (beers - 1 == 1){
+
+                    out.print(beers + " bottle" + s + " of beer on the wall,");
+                    out.println(beers + " bottle" + s + " of beer,");
+                    out.print("Take one down, pass it around,");
                     s = "";
+                    out.println(beers-1 + " bottle" + s + " of beer on the wall.\r\n");
                 }
                 else{
+                    out.print(beers + " bottle" + s + " of beer on the wall,");
+                    out.println(beers + " bottle" + s + " of beer,");
+                    out.print("Take one down, pass it around,");
                     s = "s";
+                    out.println(beers-1 + " bottle" + s + " of beer on the wall.\r\n");
                 }
 
-                out.println(beers-1 + " bottle" + s + " of beer on the wall.\r\n");
+
             }
 
 
